389c6f3574b4fadfb78b6f1238804514b2356ae3
Grant Doney <gdoney1701@gmail.com>
2019-10-10 10:34:53 -0400

    Added ability to scale spawned portal

---
+++ Creative Seminar/Assets/ScaleAbility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleAbility : MonoBehaviour
+{
+    Vector3 minScale = new Vector3(0.1f, 0.1f, 0.1f);
+    Vector3 maxScale = new Vector3(3, 3, 3);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zoomValue = Input.GetAxis("Mouse ScrollWheel");
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (zoomValue != 0)
+            {
+                transform.localScale += Vector3.one * zoomValue;
+                transform.localScale = Vector3.Max(transform.localScale, minScale);
+                transform.localScale = Vector3.Min(transform.localScale, maxScale);
+            }
+
+        }
+        
+    }
+}
--- Creative Seminar/Assets/SwitchingPhoto.cs
+++ Creative Seminar/Assets/SwitchingPhoto.cs
@@ -20,6 +20,8 @@ public class SwitchingPhoto : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float zoomValue = Input.GetAxis("Mouse ScrollWheel");
+
         if (Input.GetMouseButtonDown(0))
         {
             dist += 200;
@@ -31,22 +33,24 @@ public class SwitchingPhoto : MonoBehaviour
             dist -= 200;
             Bcam.GetComponent<CopyPositionOffset>().offset = new Vector3(dist, 0, 0);
         }
-
         if (Input.GetKeyDown(KeyCode.E) && photoaround == false)
         {
-            GameObject newPhoto = Instantiate(Photoprefab, transform.position + (transform.forward * 2), Quaternion.Euler(transform.rotation.x, transform.rotation.y + 180, transform.position.z));
+            GameObject newPhoto = Instantiate(Photoprefab, GM_Cam.transform.position + (GM_Cam.transform.forward * 2), GM_Cam.transform.rotation);
+            newPhoto.transform.Rotate(0, 180, 0, Space.World);
             newPhoto.GetComponent<Portal>().pairPortal = BPhoto;
             newPhoto.GetComponentInChildren<CopyPositionOffset>().transformToCopy = GM_Cam;
             photoaround = true;
-
-
 
         } else if (Input.GetKeyDown(KeyCode.E) && photoaround == true)
         {
             Destroy(GameObject.FindGameObjectWithTag("Photo"));
             photoaround = false;
-
+                
         }
+        //if (Input.GetKeyUp(KeyCode.E) && photoaround == false)
+        //{
+        //    photoaround = true;
+        //}
         
     }
 }
