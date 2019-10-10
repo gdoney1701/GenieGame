9164ab791641ad47198f66a606e8c2d36d3b882b
Grant <gdoney1701@gmail.com>
2019-10-09 23:44:39 -0400

    Added ability to spawn in portal

--- Creative Seminar/Assets/SwitchingPhoto.cs
+++ Creative Seminar/Assets/SwitchingPhoto.cs
@@ -6,11 +6,15 @@ public class SwitchingPhoto : MonoBehaviour
 {
     public float dist;
     public Camera Bcam;
+    public bool photoaround;
+    public GameObject Photoprefab;
+    public Transform BPhoto;
+    public Transform GM_Cam;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        photoaround = false;
     }
 
     // Update is called once per frame
@@ -26,6 +30,22 @@ public class SwitchingPhoto : MonoBehaviour
         {
             dist -= 200;
             Bcam.GetComponent<CopyPositionOffset>().offset = new Vector3(dist, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && photoaround == false)
+        {
+            GameObject newPhoto = Instantiate(Photoprefab, transform.position + (transform.forward * 2), Quaternion.Euler(transform.rotation.x, transform.rotation.y + 180, transform.position.z));
+            newPhoto.GetComponent<Portal>().pairPortal = BPhoto;
+            newPhoto.GetComponentInChildren<CopyPositionOffset>().transformToCopy = GM_Cam;
+            photoaround = true;
+
+
+
+        } else if (Input.GetKeyDown(KeyCode.E) && photoaround == true)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Photo"));
+            photoaround = false;
+
         }
         
     }
