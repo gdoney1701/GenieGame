db069de32718520d4677a879a34a44d3025a1443
Grant <gdoney1701@gmail.com>
2019-10-12 13:48:15 -0400

    Added ability to target objects through portal

--- Creative Seminar/Assets/PickingUp.cs
+++ Creative Seminar/Assets/PickingUp.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PickingUp : MonoBehaviour
 {
+    public int target;
+    public float maxPullDistance;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // creates a layer mask for the targetted layer
+        int layerMask = 1 << target;
+
+        //var mask2 = LayerMask.GetMask("Photo");
+
+        print(layerMask);
+
+
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxPullDistance, layerMask))
+            {
+                print("yup");
+            //    if (hit.distance <= maxPullDistance)
+            //    {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green, 10.0f);
+            //    } else
+            //    {
+            //        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                }
+                
+            //} else
+            //{
+            //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+            //}
+            
+        }
+
         
     }
+
+    //public bool RayMan()
+
 }
--- Creative Seminar/Assets/PlayerScript.cs
+++ Creative Seminar/Assets/PlayerScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchingPhoto : MonoBehaviour
+public class PlayerScript : MonoBehaviour
 {
     public float dist;
     public Camera Bcam;
@@ -10,6 +10,7 @@ public class SwitchingPhoto : MonoBehaviour
     public GameObject Photoprefab;
     public Transform BPhoto;
     public Transform GM_Cam;
+    public float maxPullDistance;
 
     // Start is called before the first frame update
     void Start()
@@ -47,10 +48,30 @@ public class SwitchingPhoto : MonoBehaviour
             photoaround = false;
                 
         }
-        //if (Input.GetKeyUp(KeyCode.E) && photoaround == false)
-        //{
-        //    photoaround = true;
-        //}
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            bool cam1Hit = HitDat(10, Camera.main);
+            bool cam2Hit = HitDat(9, Bcam);
+            if (cam1Hit == true && cam2Hit == true)
+            {
+                print("GOTCHA");
+            }
+        }
+
+    }
+    public bool HitDat(int target, Camera whoamI)
+    {
+        bool hitme;
+        int layerMask = 1 << target;
+        RaycastHit hit;
+        if(Physics.Raycast(whoamI.transform.position, whoamI.transform.TransformDirection(Vector3.forward), out hit, maxPullDistance, layerMask))
+        {
+            hitme = true;
+        }
+        else
+        {
+            hitme = false;
+        }
+        return hitme;
     }
 }
