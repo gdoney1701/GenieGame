7d73a4abc1c1e480ba5532dc3fccd4fcc3aa487c
Grant <gdoney1701@gmail.com>
2019-11-26 00:12:57 -0500

    Added useable shader casting
    YAY

--- Creative Seminar/Assets/Scripts/ScaleAbility.cs
+++ Creative Seminar/Assets/Scripts/ScaleAbility.cs
@@ -6,11 +6,23 @@ public class ScaleAbility : MonoBehaviour
 {
     Vector3 minScale = new Vector3(0.01f, 0.01f, 0.01f);
     Vector3 maxScale = new Vector3(5, 5, 5);
+    public GameObject PortalVisual;
+    private bool doneScaling;
+    private bool doneGrowing;
+    Renderer portalRenderer;
+    public float minimum = -0.5f;
+    public float maximum = -0.8f;
+    public float speed;
+    static float t = 0.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        portalRenderer = PortalVisual.GetComponent<Renderer>();
+        portalRenderer.material.SetFloat("Boolean_448AB95D", 0);
+        doneScaling = false;
+        doneGrowing = false;
     }
 
     // Update is called once per frame
@@ -27,6 +39,29 @@ public class ScaleAbility : MonoBehaviour
                 transform.localScale = Vector3.Min(transform.localScale, maxScale);
             }
 
+        }
+        else if (!doneScaling && !doneGrowing)
+        {
+            portalRenderer.material.SetFloat("Boolean_448AB95D", 1);
+            doneScaling = true;
+
+        }
+        if (doneScaling && !doneGrowing)
+        {
+
+            Vector3 journey = new Vector3(Mathf.Lerp(minimum, maximum, t), 0, 0);
+            t += 0.01f;
+            float goal = journey[0];
+            portalRenderer.material.SetFloat("Vector1_7431778D", goal);
+
+            if (t > 1.0f)
+            {
+                float temp = maximum;
+                maximum = minimum;
+                minimum = maximum;
+                t = 0.0f;
+                doneGrowing = true;
+            }
         }
         
     }
